f877b51a557ed134961b591ac9d6433334f5fc6b
ndukwesamuel <ndukwesamuel23@yahoo.com>
2022-08-31 15:22:57 +0100

    new

--- config/db.js
+++ config/db.js
@@ -14,8 +14,8 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI_LOCAL.toString());
-    console.log(process.env.MONGO_URI_LOCAL);
+    const conn = await mongoose.connect(process.env.MONGO_URI.toString());
+    console.log(process.env.MONGO_URI);
     console.log(`MongoDB Connected:`);
   } catch (error) {
     console.log(error);
